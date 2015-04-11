98919d3385d7cf2110d89cd87b0eeafcaf26388b
Andrew Melis <andrewmelis@gmail.com>
2015-04-11 14:48:13 -0500

    remove tromey(?) from package archives

--- init.el
+++ init.el
@@ -7,8 +7,6 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
