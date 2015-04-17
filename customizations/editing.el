03b618bffd8ca27280f5c60eb63297fcb2d5ec24
Andrew Melis <andrewmelis@gmail.com>
2015-04-17 09:40:56 -0500

    remove company-colors

--- customizations/editing.el
+++ customizations/editing.el
@@ -12,17 +12,6 @@
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol))
 
-;; Company colors
-(require 'color)
-  
-(let ((bg (face-attribute 'default :background)))
-  (custom-set-faces
-   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
-
 ;; Highlights matching parenthesis
 (show-paren-mode 1)
 
