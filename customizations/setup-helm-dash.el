bbbd5aba6a45550c3b0f378b6cbc312a95b36dfe
Andrew Melis <andrewmelis@gmail.com>
2015-04-17 09:42:56 -0500

    add helm-dash package

--- customizations/setup-helm-dash.el
+++ customizations/setup-helm-dash.el
@@ -2,5 +2,20 @@
 ;; Helm-Dash
 ;;;;
 
+(require 'helm-dash)
+
 ;; use eww to browse docsets
 (setq helm-dash-browser-func 'eww)
+
+;; common docsets
+
+;; fancy mode appropriate local docsets
+
+;; TODO fix this
+;; (defun ruby-doc ()
+;;   (interactive)
+;;   (setq-local helm-dash-docsets '("Ruby")))
+
+(add-hook 'ruby-mode-hook '(lambda () (setq-local helm-dash-docsets '("Ruby" "Ruby on Rails"))))
+(add-hook 'clojure-mode-hook '(lambda () (setq-local helm-dash-docsets '("Clojure"))))
+
