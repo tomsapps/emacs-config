fc1ce69989219c4b7b015abe82ab212c8feb6974
Andrew Melis <andrewmelis@gmail.com>
2015-05-30 16:30:47 -0500

    add file-name-on-clipboard function

--- customizations/editing.el
+++ customizations/editing.el
@@ -70,3 +70,17 @@
     (quit nil)))
 
 ;; (setq electric-indent-mode nil)
+
+
+(defun file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
+
