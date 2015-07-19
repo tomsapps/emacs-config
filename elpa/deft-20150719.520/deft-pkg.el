0db5879b9a196a3fbba071cf74470aa95933d598
Andrew Melis <andrewmelis@gmail.com>
2015-07-19 13:44:34 -0500

    update packages

---
+++ elpa/deft-20150719.520/deft-pkg.el
@@ -0,0 +1,1 @@
+(define-package "deft" "20150719.520" "quickly browse, filter, and edit plain text notes" 'nil :url "http://jblevins.org/projects/deft/" :keywords '("plain text" "notes" "simplenote" "notational velocity"))
