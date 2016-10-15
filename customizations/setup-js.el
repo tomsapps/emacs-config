7ab7374443cc16484a252f58d1a129f19d45708b
Jones Melton <shavah@gmail.com>
2016-10-15 15:07:26 -0500

    Add eslint and flycheck

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,7 +1,20 @@
 ;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 (add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'html-mode-hook 'subword-mode)
+
+;; disable jshint
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+;; eslint instead
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(setq-default flycheck-temp-prefix ".flycheck")
+
 (setq js-indent-level 2)
 (eval-after-load "sgml-mode"
   '(progn
@@ -9,7 +22,18 @@
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 
+     ;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
+;; make sure shell ENV is pulled in
+(when (memq window-system '(mac ns))
+ (exec-path-from-shell-initialize))
 ;; coffeescript
 (add-to-list 'auto-mode-alist '("\\.coffee.erb$" . coffee-mode))
 (add-hook 'coffee-mode-hook 'subword-mode)
